5cd72ac92b653a6382f4659f71e930743838edfa
mfbrewster <michael.f.brewster@gmail.com>
2016-02-25 11:47:56 -0500

    Added given_name and surname columns to users table

--- scripts/change-password.sh
+++ scripts/change-password.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl --include --request PATCH http://localhost:3000/change-password/$ID \
-  --header "Authorization: Token token=$TOKEN" \
+curl --include --request PATCH http://localhost:3000/change-password/1 \
+  --header "Authorization: Token token=fe258500871175e73135009de70b9504" \
   --header "Content-Type: application/json" \
   --data '{
     "passwords": {
-      "old": "an example password",
-      "new": "super sekrit"
+      "old": "asdf",
+      "new": "1234"
     }
   }'
--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl --include --request DELETE http://localhost:3000/sign-out/$ID \
-  --header "Authorization: Token token=$TOKEN"
+curl --include --request DELETE http://localhost:3000/sign-out/1 \
+  --header "Authorization: Token token=fe258500871175e73135009de70b9504"
